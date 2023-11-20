b1819f8aac815d61e4738a3fb0fe651aebfa8649
Yasshhh12 <prasyast152@gmail.com>
2023-11-20 23:42:35 +0530

    writing some logic in js

---
+++ Todo.js
@@ -0,0 +1,16 @@
+const searchBox = document.querySelector(".search");
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+
+function task(){
+    
+}
+
+function main(){
+    searchBox.addEventListener('input',function(){
+        task();
+    });
+}
+
+main();
