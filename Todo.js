551005b985c54fda23d2a1b20ff98915d4bed7c9
Yasshhh12 <prasyast152@gmail.com>
2023-11-28 09:24:39 +0530

    Adding final js.Done with the project

--- Todo.js
+++ Todo.js
@@ -1,16 +1,39 @@
-const searchBox = document.querySelector(".search");
-const taskInput = document.getElementById("taskInput");
-const addButton = document.getElementById("addButton");
-const taskList = document.getElementById("taskList");
+ const inputBox = document.getElementById("input-box");
+ const listContainer = document.getElementById("list-container");
 
-function task(){
-    
+ function addTask(){
+    if(inputBox.value == ''){
+        alert("You must write something!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value= "";
+    saveData();
+ }
+
+ listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},  false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
 }
 
-function main(){
-    searchBox.addEventListener('input',function(){
-        task();
-    });
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 
-main();
+showTask();
